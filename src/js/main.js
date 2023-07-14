3b47f409100e46c3852dbe3375046509d9890974
Amir Shammasov <73101144+ShammasovAmir@users.noreply.github.com>
2023-07-14 20:40:28 +0300

    Started adding products

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 import Swiper from "swiper"
 import Pagination from "swiper/modules/pagination.mjs"
 import Autoplay from "swiper/modules/autoplay.mjs"
+import Navigation from "swiper/modules/navigation.mjs"
 
-Swiper.use([Pagination, Autoplay])
+Swiper.use([Pagination, Autoplay, Navigation])
 
 const bannerSwiper = new Swiper(".banner__swiper", {
   loop: true,
@@ -21,4 +22,14 @@ const advantagesSwiper = new Swiper(".advantages__swiper", {
   loop: true,
   slidesPerView: 5,
   spaceBetween: 24,
+})
+
+const productsSwiper = new Swiper(".products__swiper", {
+  // loop: true,
+  slidesPerView: 4,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 })
